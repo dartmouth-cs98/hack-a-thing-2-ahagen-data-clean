bbfaa33ab4d658276da724df705172f16158100e
Ali Hagen <alihagen@Alis-MacBook-Pro-3.local>
2019-09-30 14:23:40 -0400

    frequencies

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from urllib.parse import urlparse
 
 # Open our file
 with open('hist.txt') as f:
@@ -13,15 +14,29 @@ raw_data = [line.split('|', 1) for line in [x.strip() for x in content]]
 data = pd.DataFrame(raw_data, columns=['datetime', 'url'])
 
 #initalize the copy
-data2019 = data
+data2019 = data.copy()
 # loop through the dataframe
 for index, row in data.iterrows():
 	if "2019" not in row['datetime']:
 		# remove row if it does not contain 2019
 		data2019 = data2019.drop(index)
 
-print(data2019)
+#print(data2019)
 
 # convert time stamp to datetime format
 data2019.datetime = pd.to_datetime(data2019.datetime)
-print(data2019.datetime[0])
+
+# initialize for 2019 data with parsed urls
+data2019p = data2019.copy()
+
+parser = lambda u: urlparse(u).netloc
+data2019p.url = data2019p.url.apply(parser)
+
+# Aggregate domain entries
+site_frequencies = data2019p.url.value_counts().to_frame()
+# Make the domain a column
+site_frequencies.reset_index(level=0, inplace=True)
+# Rename columns to appropriate names
+site_frequencies.columns = ['domain', 'count']
+# Display top 2
+print(site_frequencies.head(20))
